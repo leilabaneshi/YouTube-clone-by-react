f6c5d05926c4cd9e9ebd509cfd9642eeba6857ae
Leila Baneshi <baneshi.leila@gmail.com>
2020-11-18 16:47:14 +0100

    create Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-
+import RecommendedVideo from './RecommendedVideo';
+import Sidebar from './Sidebar';
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className="app__page">
+        <Sidebar />
+
+      </div>
+
 
     </div>
   );
---
+++ src/RecommendedVideo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RecommendedVideo.css';
+
+function RecommendedVideo() {
+    return (
+        <div className='recommendedVideo'>
+            <h1>recom</h1>
+        </div>
+    )
+}
+
+export default RecommendedVideo
